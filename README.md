25398195cdbbe67ba7cc701a4e39867eb07872c2
Juan5987 <juan.machado.5987@gmail.com>
2022-03-04 12:13:07 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# react-google-auth
+# REACT GOOGLE AUTH
+coming
---
+++ src/components/App/index.js
@@ -0,0 +1,11 @@
+import Login from 'components/Login'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
